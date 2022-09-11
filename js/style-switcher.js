be4cbb764deb78000921fcaaef4b05900a9a0817
Sahil Ahmed <sahil.khn338@gmail.com>
2022-09-11 13:04:25 +0500

    ..

---
+++ js/style-switcher.js
@@ -0,0 +1,38 @@
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+const styleSwitcher = document.querySelector(".style-switcher");
+
+styleSwitcherToggle.addEventListener("click", () => {
+  styleSwitcher.classList.toggle("open");
+});
+
+window.addEventListener("scroll", () => {
+  if (styleSwitcher.classList.contains("open")) {
+    styleSwitcher.classList.remove("open");
+  }
+});
+// theme color
+const alternateStyle = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+  alternateStyle.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
+
+// theme light and dark mode
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  document.body.classList.toggle("dark");
+});
+window.addEventListener("laod", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
